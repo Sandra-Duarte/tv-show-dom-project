9c51e0c62c395f161b2a059ac0568fb0f34933f8
Sandra-Duarte <106591234+Sandra-Duarte@users.noreply.github.com>
2022-11-09 16:13:52 +0000

    same javaScript

--- script.js
+++ script.js
@@ -6,7 +6,49 @@ function setup() {
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+ // rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+ 
+ //Add elements 
+ getAllEpisodes().forEach((episode) =>{
+  const card = document.createElement("div");
+  card.classList.add("card");
+  const cardHeader = document.createElement("div");
+  cardHeader.classList.add("cardHeader");
+  const title = document.createElement("h3");
+  const season = document.createElement("h4");
+  const image = document.createElement("imag");
+  const summary = document.createElement("p");
+    summary.classList.add("summary");
+
+    // Add content to your elements 
+    title.textContent = episode.name;
+    season.textContent = `S${episode.season
+      .toString()
+      .padStart(2, "0")}E${episode.number.toString().padStart(2, "0")}`;
+    image.src = episode.image.medium;
+    summary.innerHTML = episode.summary;
+
+    // Append
+    cardHeader.appendChild(title);
+    cardHeader.appendChild(season);
+    cardHeader.classList.add("header-title");
+    card.appendChild(cardHeader);
+    card.appendChild(image);
+    card.appendChild(summary);
+    rootElem.appendChild(card);
+
+ });
+
+
 }
 
 window.onload = setup;
+
+
+/*let episodes;
+
+
+window.onload = () => {
+  episodes = getAllEpisodes()
+}*/
+
